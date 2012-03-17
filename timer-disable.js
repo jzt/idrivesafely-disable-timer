82d9b530d70fbbbb62abfaec990294854eeb4ecb
Dean Chen <dean@ocirs.com>
2012-03-17 00:58:17 -0400

    Added jquery, change timer to show 0

--- timer-disable.js
+++ timer-disable.js
@@ -2,3 +2,7 @@ var scriptContent = "vTimeReq = 0;"
 var script = document.createElement('script')
 script.appendChild(document.createTextNode(scriptContent));
 document.getElementsByTagName('head')[0].appendChild(script);
+
+$(function() {
+    $("form[name='timer'] > input").val("0");
+})
