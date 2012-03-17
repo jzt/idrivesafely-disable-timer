2345d226085ec7f26df4629b32fbe005872a13ea
Dean Chen <dean@ocirs.com>
2012-03-17 02:22:47 -0400

    Added feature to show when script is active.

--- timer-disable.js
+++ timer-disable.js
@@ -1,3 +1,5 @@
+chrome.extension.sendRequest({setIconPath: "icon-color.png"}, function(){});
+
 var scriptContent = "vTimeReq = 0;"
 var script = document.createElement('script')
 script.appendChild(document.createTextNode(scriptContent));
