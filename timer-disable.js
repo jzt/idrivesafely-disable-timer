704c76448b4292362500a8b9e8855f2efe061251
Dean Chen <dean@ocirs.com>
2012-03-17 00:02:01 -0400

    Working extension.

---
+++ timer-disable.js
@@ -0,0 +1,4 @@
+var scriptContent = "vTimeReq = 0;"
+var script = document.createElement('script')
+script.appendChild(document.createTextNode(scriptContent));
+document.getElementsByTagName('head')[0].appendChild(script);
